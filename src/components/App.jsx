0e37d5ecdb2ccf217c4885fa99274966d817a8a6
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-07-12 15:02:37 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {ContactForm} from './ContactForm/ContactForm';
+import {ContactList} from './ContactList/ContactList';
+import {Filter} from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleFormSubmit = (name, number) => {
+    const isContactExists = contacts.some(
+      (contact) =>
+        contact.name.toLowerCase() === name.toLowerCase() ||
+        contact.number === number
+    );
+
+    if (isContactExists) {
+      alert(`${name} or ${number} is already in contacts`);
+    } else {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      setContacts((prevContacts) => [newContact, ...prevContacts]);
+    }
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleContactDelete = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
+  };
+
+  const getFilteredContacts = () => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handleFormSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} changeFilterInput={handleFilterChange} />
+      <ContactList contacts={filteredContacts} deleteContact={handleContactDelete} />
+    </section>
   );
 };
+
+App.propTypes = {
+  contacts: PropTypes.array,
+  filter: PropTypes.string,
+};
+
+
+
+
